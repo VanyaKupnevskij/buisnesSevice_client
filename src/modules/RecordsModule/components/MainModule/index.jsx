72d36b6cc2a4dd46d99ac6f85e38d0bf4a07b083
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-06-06 14:23:40 +0300

    decomposition records module

--- src/modules/RecordsModule/components/MainModule/index.jsx
+++ src/modules/RecordsModule/components/MainModule/index.jsx
@@ -1,18 +1,24 @@
+import { useEffect, useState } from 'react';
 import styles from './style.module.scss';
 
 import Loading from '../../../../ui/Loading';
 import Modal from '../../../../components/Modal';
 import Table from '../../../../components/Table';
 
-import { useHttp } from '../../../../hooks/http.hook';
-import { useEffect, useState } from 'react';
-import { useAuth } from '../../../../hooks/auth.hook';
-import { useProject } from '../../../../hooks/projects.hook';
+import tamplateDataModal, { titles } from '../../constants/tamplateDataModal';
+import { useRecordsHttp } from '../../helpers/recordsHttp.hook';
 
 function RecordsModule() {
-  const { loading, request, error } = useHttp();
-  const { selectedId } = useProject();
-  const { token } = useAuth();
+  const {
+    loading,
+    error,
+    deleteRecord,
+    createRecord,
+    updateRecord,
+    requestWorkers,
+    requestRecords,
+  } = useRecordsHttp();
+
   const [records, setRecords] = useState([]);
   const [workers, setWorkers] = useState([]);
   const [renderList, setRenderList] = useState([]);
@@ -24,117 +30,46 @@ function RecordsModule() {
     end_date: undefined,
   });
 
-  const titles = ['Дата', 'Рахунок', 'Джерело', 'Прибуток', 'Витрати', 'Оплачено'];
-  const tamplateDataModal = {
-    id: { name: 'id', title: 'id', value: null, type: 'text', type_display: 'none' },
-    date: { name: 'date', title: 'Дата', value: null, type: 'date', type_display: 'all' },
-    money_account: {
-      name: 'money_account',
-      title: 'Рахунок',
-      value: 'Не задано',
-      type: 'select',
-      type_display: 'all',
-      options: [
-        { key: 'ФОП', value: 'ФОП' },
-        { key: 'Monobank', value: 'Monobank' },
-        { key: 'Privatebank', value: 'Privatebank' },
-        { key: 'Готівка', value: 'Готівка' },
-        { key: 'Інше', value: 'Інше' },
-        { key: null, value: 'Не задано' },
-      ],
-    },
-    comment: {
-      name: 'comment',
-      title: 'Коментар',
-      value: '',
-      type: 'text',
-      type_display: 'all',
-      multiple: true,
-    },
-    source_from: {
-      name: 'source_from',
-      title: 'Джерело',
-      value: 'Не задано',
-      type: 'select',
-      type_display: 'all',
-      options: [
-        { key: 'Реклама', value: 'Реклама' },
-        { key: 'Тарегет', value: 'Тарегет' },
-        { key: 'Зарплатня', value: 'Зарплатня' },
-        { key: 'Підписка', value: 'Підписка' },
-        { key: 'Інше', value: 'Інше' },
-        { key: null, value: 'Не задано' },
-      ],
-    },
-    income: {
-      name: 'income',
-      title: 'Прибуток',
-      value: 0,
-      type: 'number',
-      type_display: 'all',
-    },
-    costs: {
-      name: 'costs',
-      title: 'Витрати',
-      value: 0,
-      type: 'number',
-      type_display: 'all',
-    },
-    already_paid: {
-      name: 'already_paid',
-      title: 'Оплачено',
-      value: 0,
-      type: 'number',
-      type_display: 'all',
-    },
-    worker_full_name: {
-      name: 'worker_full_name',
-      title: "Ім'я працівника",
-      value: 'Не задано',
-      type: 'select',
-      type_display: 'all',
-      options: [],
-    },
-    worker_money_account: {
-      name: 'worker_money_account',
-      title: 'Рахунок працівника',
-      value: 'Не задано',
-      type: 'select',
-      type_display: 'readonly',
-      options: [
-        { key: 'ФОП', value: 'ФОП' },
-        { key: 'Monobank', value: 'Monobank' },
-        { key: 'Privatebank', value: 'Privatebank' },
-        { key: 'Готівка', value: 'Готівка' },
-        { key: 'Інше', value: 'Інше' },
-        { key: null, value: 'Не задано' },
-      ],
-    },
-    worker_realm: {
-      name: 'worker_realm',
-      title: 'Спеціалізація працівника',
-      value: 'Не задано',
-      type: 'select',
-      type_display: 'readonly',
-      options: [
-        { key: 'Сео', value: 'Сео' },
-        { key: 'Таргетолог', value: 'Таргетолог' },
-        { key: 'Менеджер', value: 'Менеджер' },
-        { key: 'Діректор', value: 'Діректор' },
-        { key: 'Дизайнер', value: 'Дизайнер' },
-        { key: 'Программіст', value: 'Программіст' },
-        { key: 'Інше', value: 'Інше' },
-        { key: null, value: 'Не задано' },
-      ],
-    },
-    worker_salary: {
-      name: 'worker_salary',
-      title: 'Зарплатня працівника',
-      value: null,
-      type: 'number',
-      type_display: 'readonly',
-    },
-  };
+  useEffect(() => {
+    loadRecords();
+  }, []);
+
+  async function loadRecords() {
+    try {
+      const params = {};
+      if (filterDate.start_date) {
+        params.start_date = filterDate.start_date;
+      }
+      if (filterDate.end_date) {
+        params.end_date = filterDate.end_date;
+      }
+
+      const responceRecords = await requestRecords(params);
+      const responceWorkers = await requestWorkers();
+
+      setRecords(responceRecords);
+      setWorkers(responceWorkers);
+
+      const formatedList = makeFormatedList(responceRecords.resultRecords);
+
+      setRenderList(formatedList);
+    } catch (e) {}
+  }
+
+  function makeFormatedList(inputList) {
+    const resultList = inputList.map((record) => {
+      return {
+        date: new Date(record.date).toLocaleDateString(),
+        money_account: record.money_account,
+        source_from: record.source_from,
+        income: record.income + ' грн.',
+        costs: record.costs + ' грн.',
+        already_paid: record.already_paid + ' грн.',
+      };
+    });
+
+    return resultList;
+  }
 
   function handleClickRow(index) {
     setRegimModal('update');
@@ -166,79 +101,6 @@ function RecordsModule() {
     setShowModal(true);
   }
 
-  async function loadRecords() {
-    try {
-      const params = { projects_id: selectedId };
-      if (filterDate.start_date) {
-        params.start_date = filterDate.start_date;
-      }
-      if (filterDate.end_date) {
-        params.end_date = filterDate.end_date;
-      }
-
-      const responceRecords = await request({
-        url: '/records',
-        method: 'get',
-        bearerToken: token,
-        params,
-      });
-      const responceWorkers = await request({
-        url: '/workers',
-        method: 'get',
-        bearerToken: token,
-      });
-
-      setRecords(responceRecords);
-      setWorkers(responceWorkers);
-
-      const formatedList = responceRecords.resultRecords.map((record) => {
-        return {
-          date: new Date(record.date).toLocaleDateString(),
-          money_account: record.money_account,
-          source_from: record.source_from,
-          income: record.income + ' грн.',
-          costs: record.costs + ' грн.',
-          already_paid: record.already_paid + ' грн.',
-        };
-      });
-
-      setRenderList(formatedList);
-    } catch (e) {}
-  }
-  async function updateRecord(newDataRecord) {
-    try {
-      const responceRecord = await request({
-        url: '/records/update/' + newDataRecord.id,
-        method: 'put',
-        bearerToken: token,
-        data: newDataRecord,
-      });
-    } catch (e) {}
-  }
-  async function createRecord(newDataRecord) {
-    try {
-      const responceRecord = await request({
-        url: '/records/create',
-        method: 'post',
-        bearerToken: token,
-        data: newDataRecord,
-      });
-    } catch (e) {}
-  }
-  async function deleteRecord(id) {
-    try {
-      await request({
-        url: '/records/' + id,
-        method: 'delete',
-        bearerToken: token,
-      });
-    } catch (e) {}
-  }
-
-  useEffect(() => {
-    loadRecords();
-  }, []);
-
   function handleCloseModal() {
     setShowModal(false);
   }
@@ -251,7 +113,7 @@ function RecordsModule() {
     await loadRecords();
   }
 
-  async function handleSubmitModal(newValues, regim) {
+  async function handleSubmitModal(newValues) {
     const formatedValues = {};
     let income = null;
     let costs = null;
@@ -290,7 +152,6 @@ function RecordsModule() {
     if (regimModal === 'update') {
       await updateRecord(formatedValues);
     } else {
-      formatedValues.projects_id = selectedId;
       await createRecord(formatedValues);
     }
 
