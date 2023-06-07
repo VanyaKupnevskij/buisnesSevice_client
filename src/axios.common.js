18959f88f680204ff09d1d03d311cad7814f0fc3
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-06-07 18:31:53 +0300

    change baseUrl

--- src/axios.common.js
+++ src/axios.common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:4444/api',
+  baseURL: 'http://localhost/api',
   headers: {
     'Content-type': 'application/json',
   },
